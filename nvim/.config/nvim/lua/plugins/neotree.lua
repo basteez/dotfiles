722a5761098101cabcf981d198dff5094c33ad36
Tiziano Basile <tiziano.basile@nearform.com>
2025-06-10 16:12:50 +0200

    feat(nvim-neotree): add hidden files vilsibility

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -9,7 +9,28 @@ return {
   },
   lazy = false,
   opts = {
-    -- fill any relevant options here
+    filesystem = {
+      filtered_items = {
+        visible = false, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_hidden = true, -- only works on Windows for hidden files/directories
+        hide_by_name = {
+          ".DS_Store",
+          "thumbs.db"
+        },
+        hide_by_pattern = {
+          --"*.meta",
+        },
+        always_show = { -- remains visible even if other settings would normally hide it
+          --".gitignored",
+        },
+        never_show = { -- remains hidden even if visible is toggled to true
+          --".DS_Store",
+          --"thumbs.db"
+        },
+      },
+    },
   },
   config = function(_, opts)
     require("neo-tree").setup(opts)
