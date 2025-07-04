13e35c8ec79bb3d169c2d25ec05029113807ca63
Tiziano Basile <tiziano.basile@nearform.com>
2025-07-04 16:20:27 +0200

    feat(doom): edit font

--- doom/.config/doom/custom.el
+++ doom/.config/doom/custom.el
@@ -10,4 +10,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#282c34" :foreground "#bbc2cf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 160 :width normal :foundry "nil" :family "Menlo")))))
